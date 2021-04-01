d791117cc995d641ab74f12f209b3d417159db40
Ravneet kaur <rvkaurrv9@gmail.com>
2021-04-01 14:51:03 +0530

    Add files via upload

---
+++ management/src/main/java/com/employee/management/Management.java
@@ -0,0 +1,14 @@
+package com.employee.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Management {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      SpringApplication.run(Management.class);
+	}
+
+}
